a2c715c9681569b784a70a3cd810a01221a2aa02
Igor Barsi <ibarsi92@gmail.com>
2018-10-11 09:22:05 -0400

    Removed flow related things, updated VS Code extensions list, updated settings for newer VS Code versions.

--- vscode.sh
+++ vscode.sh
@@ -4,7 +4,6 @@
 
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension ms-python.python
-code --install-extension flowtype.flow-for-vscode
 code --install-extension mkaufman.HTMLHint
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension shinnn.stylelint
@@ -14,11 +13,10 @@ code --install-extension christian-kohler.npm-intellisense
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.editorconfig
 code --install-extension esbenp.prettier-vscode
-code --install-extension asvetliakov.snapshot-tools
 code --install-extension ban.spellright
 code --install-extension bierner.markdown-preview-github-styles
 code --install-extension wayou.vscode-todo-highlight
-code --install-extension alexdima.copy-relative-path
-code --install-extension wangtao0101.vscode-js-import
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension yahya-gilany.vscode-terminal-launcher
+code --install-extension eg2.tslint
+code --install-extension davidanson.vscode-markdownlint
