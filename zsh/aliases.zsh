2100eea45a8a7405c703feaa79cde2385986fe33
Igor Barsi <ibarsi92@gmail.com>
2026-04-10 16:48:48 -0400

    🧑‍💻 (omlx): Add alias to start server

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,6 +27,7 @@ alias ccyolo="claude --dangerously-skip-permissions"
 
 # Upgrade AI coding tools
 alias aiup="brew upgrade claude-code codex opencode"
+alias omlxs='omlx serve --model-dir ~/.omlx/models --port 1234'
 
 # Mise
 alias ms="mise"
